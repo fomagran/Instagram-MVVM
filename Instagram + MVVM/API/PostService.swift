17bfd82424806573cc0372949c75a53b280a0a5a
Fomagran <fomagran6@naver.com>
2020-11-20 00:19:58 +0900

    포스트 올렸을 때 모든 팔로워 유저에게 포스트아이디 추가해주기 (Cloud Firebase Function을 이용해야하지만 MVP버전으로 그냥 함)

--- Instagram + MVVM/API/PostService.swift
+++ Instagram + MVVM/API/PostService.swift
@@ -20,7 +20,8 @@ struct PostService {
                         "ownerImageUrl":user.profileImageUrl,
                         "ownerUsername":user.username] as [String:Any]
             
-            COLLECTION_POSTS.addDocument(data: data, completion: completion)
+            let docRef = COLLECTION_POSTS.addDocument(data: data, completion: completion)
+            self.updateUserFeedAfterPost(postId:docRef.documentID)
         }
     }
     
@@ -97,17 +98,31 @@ struct PostService {
         }
     }
     
-    static func updateUserFeedFolloing(user:User) {
+    static func updateUserFeedFolloing(user:User,didFollow:Bool) {
         guard let uid = Auth.auth().currentUser?.uid else {return}
         let query = COLLECTION_POSTS.whereField("ownerUid", isEqualTo: user.uid)
         query.getDocuments { (snapshot, error) in
             guard let documents = snapshot?.documents else {return}
             let docIds = documents.map{($0.documentID)}
             docIds.forEach { (id) in
-                COLLECTION_USERS.document(uid).collection("user-feed").document(id).setData([:])
+                if didFollow {
+                    COLLECTION_USERS.document(uid).collection("user-feed").document(id).setData([:])
+                }else{
+                    COLLECTION_USERS.document(uid).collection("user-feed").document(id).delete()
+                }
             }
         }
-            
         }
+    
+    private static func updateUserFeedAfterPost(postId:String) {
+        guard let uid = Auth.auth().currentUser?.uid else { return  }
+        COLLECTION_FOLLOWERS.document(uid).collection("user-followers").getDocuments { (snapshot, error) in
+            guard let documents = snapshot?.documents else {return}
+            documents.forEach { (document) in
+                COLLECTION_USERS.document(document.documentID).collection("user-feed").document(postId).setData([:])
+            }
+            COLLECTION_USERS.document(uid).collection("user-feed").document(postId).setData([:])
+        }
+    }
     }
 
